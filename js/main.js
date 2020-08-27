8c113b3af4d29220948dc6f2de2538cf63cae120
Marian <marianosisla@gmail.com>
2020-08-27 10:36:38 -0300

    Clase nro 3 - se arma la PWA

--- js/main.js
+++ js/main.js
@@ -1,54 +1,113 @@
 let listaProductos = [
-
-    {nombre:'Pan',cantidad:1,precio:3.4},
-    {nombre:'Fideos',cantidad:1,precio:5.5},
-    {nombre:'Chocolates',cantidad:5,precio:6}
-
+    { nombre: 'Pan', cantidad: 2, precio: 2.4 },
+    { nombre: 'Leche', cantidad: 5, precio: 11.4 },
+    { nombre: 'Fideos', cantidad: 4, precio: 5.4 },
 ]
+let ul;
+let listaCreada = false;
 
-function renderList() {
-
-    let ul = document.createElement('ul');
-    ul.classList.add('demo-list-icon','mdl-list');
-
-    ul.innerHTML='';
-
-    listaProductos.forEach((producto,index) => {
-
-        ul.innerHTML +=
-        `<li class="mdl-list__item">
-            <span class="mdl-list__item-primary-content w-10">
-                <i class="material-icons mdl-list__item-icon">shopping_cart</i>
-            </span>
-            <span class="mdl-list__item-primary-content w-10">
-                ${producto.nombre}
-            </span>
-            <span class="mdl-list__item-primary-content w-30">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample4">
-                    <label class="mdl-textfield__label" for="sample4">Cantidad...</label>
-                    <span class="mdl-textfield__error">Input is not a number!</span>
-                </div>
-            </span>
-            <span class="mdl-list__item-primary-content w-30">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample4">
-                    <label class="mdl-textfield__label" for="sample4">Precio...</label>
-                    <span class="mdl-textfield__error">Input is not a number!</span>
-                </div>
-            </span>
-
-            <span class="mdl-list__item-primary-content w-20">
-                <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
-                    <i class="material-icons">add</i>
-                </button>
-            </span>
-
-        </li>`;
-    });
-
-    document.getElementById('lista').appendChild(ul);
-
+function borrarProducto(index) {
+    listaProductos.splice(index, 1);
+    renderLista();
+}
+function cambiarCantidad(index, e) {
+    let cantidad = Number(e.value);
+    listaProductos[index].cantidad = cantidad;
+}
+function cambiarPrecio(index, e) {
+    let precio = Number(e.value);
+    listaProductos[index].precio = precio;
 }
 
-renderList();
+function configurarListeners() {
+    document.getElementById('btn_entrada_producto').addEventListener('click', () => {
+        let prod = document.getElementById('ingreso_producto').value;
+        if (prod !== '') {
+            listaProductos.push({
+                nombre: prod,
+                cantidad: 1,
+                precio: 0
+            });
+            document.getElementById('ingreso_producto').value = '';
+            document.getElementById('ingreso_producto').focus();
+            renderLista();
+        }
+    });
+    document.getElementById('btn_borrar_todos_productos').addEventListener('click', () => {
+        listaProductos = [];
+        renderLista();
+    })
+}
+
+
+function renderLista() {
+
+    if (!listaCreada) {
+        ul = document.createElement('ul');
+        ul.classList.add('demo-list-icon', 'mdl-list', 'w-100');
+    }
+
+    ul.innerHTML = '';
+
+    listaProductos.forEach((producto, index) => {
+        ul.innerHTML +=
+            `
+        <ul class="demo-list-icon mdl-list">
+            <li class="mdl-list__item">
+                <span class="mdl-list__item-primary-content w-10">
+                    <i class="material-icons">shopping_cart</i>
+                </span>
+                <span class="mdl-list__item-primary-content w-30">
+                    ${producto.nombre}
+                </span>
+                <span class="mdl-list__item-primary-content w-20">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input onchange="cambiarCantidad(${index}, this)" class="mdl-textfield__input" type="text" id="cantidad-${index}" value="${producto.cantidad}">
+                        <label class="mdl-textfield__label" for="cantidad-${index}">Cantidad</label>
+                    </div>
+                </span>
+                <span class="mdl-list__item-primary-content w-20 ml-20">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input onchange="cambiarPrecio(${index}, this)"  class="mdl-textfield__input" type="text" id="precio-${index}"  value="${producto.precio}">
+                        <label class="mdl-textfield__label" for="precio-${index}">Precio</label>
+                    </div>
+                </span>
+                <span class="mdl-list__item-primary-content w-20">
+                                <button onclick="borrarProducto(${index})"
+                                    class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored ml-20">
+                                    <i class="material-icons">remove_shopping_cart</i>
+                                </button>
+                            </span>
+                        </li>
+                    </ul>
+        `
+    })
+
+    if (!listaCreada) {
+        document.getElementById('lista').appendChild(ul);
+    } else {
+        componentHandler.upgradeElements(ul);
+    }
+
+    listaCreada = true;
+}
+
+function registrarServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            this.navigator.serviceWorker.register('./sw.js').then(reg => {
+                console.log('service worker registrado correctamente');
+            }).catch(err => {
+                console.error('service worker falló en registrarse');
+            })
+        })
+    }
+}
+
+function start() {
+    renderLista();
+    configurarListeners();
+    registrarServiceWorker();
+}
+
+window.addEventListener('DOMContentLoaded', start)
