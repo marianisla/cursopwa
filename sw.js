93779ac3d2abd335cead555fc665aa147b085418
Marian <marianosisla@gmail.com>
2020-09-06 12:13:47 -0300

    clase 4 service workers

--- sw.js
+++ sw.js
@@ -5,5 +5,27 @@ self.addEventListener('activate', e => {
     console.log('sw activate');
 })
 self.addEventListener('fetch', e => {
-    console.log('sw activate');
+    
+    const request = e.request;
+    let response;
+
+    if(!self.navigator.onLine){
+        response = new Response('NO HAY INTERNET', {headers: {'content-type': 'text/plain'}});
+        e.respondWith(response);
+        return;
+    }
+
+    //cambio de ícono por otro
+    if (request.url.includes('logo.png')) {
+
+        response = fetch('images/icons/logo_offline.png');
+
+    } else {
+        
+        // caso contrario que busque normalmente el recurso.
+        response = fetch(request);
+    }
+
+    e.respondWith(response);
+
 })
